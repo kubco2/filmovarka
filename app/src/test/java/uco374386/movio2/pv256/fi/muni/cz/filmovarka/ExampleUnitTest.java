45eb0a9a43b97597ba03f8ce44d9ee0aae901930
user <user@eduroam44-170.fi.muni.cz>
2016-09-20 14:00:32 +0200

    Initial commit
    Added initial project structure with StrictMode on

---
+++ app/src/test/java/uco374386/movio2/pv256/fi/muni/cz/filmovarka/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package uco374386.movio2.pv256.fi.muni.cz.filmovarka;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
