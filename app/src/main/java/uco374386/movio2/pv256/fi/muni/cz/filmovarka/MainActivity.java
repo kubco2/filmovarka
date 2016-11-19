89b9f3c445603e6a1a65a324c1a6ff94ccd7f9ed
Jakub Janco <kubco2@gmail.com>
2016-11-19 15:39:28 +0100

    categories menu selection

--- app/src/main/java/uco374386/movio2/pv256/fi/muni/cz/filmovarka/MainActivity.java
+++ app/src/main/java/uco374386/movio2/pv256/fi/muni/cz/filmovarka/MainActivity.java
@@ -14,7 +14,13 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener{
@@ -22,6 +28,8 @@ public class MainActivity extends AppCompatActivity
     private static final String TAG = MainActivity.class.getSimpleName();
 
     public static final String SECONDARY_THEME = "secondary_theme";
+    public static final String PROP_DISABLED_CATEGORIES = "disabled_categories";
+    public static final String EXTR_CAT_ID = "category_id";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +54,14 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Set<String> disabled = getDisabledCategories();
+        for(int i = 0; i < categories_id.length; i++) {
+            Menu menu = navigationView.getMenu().getItem(0).getSubMenu();
+            MenuItem item = menu.add(categories_names[i]);
+            item.setIntent(new Intent(categories_id[i]));
+            setupNavItem(item, categories_id[i], disabled.contains(categories_id[i]));
+        }
 
         if (findViewById(R.id.details) != null) {
             if(savedInstanceState != null) {
@@ -87,10 +103,38 @@ public class MainActivity extends AppCompatActivity
         return netInfo != null && netInfo.isConnected();
     }
 
+    public Set<String> getDisabledCategories() {
+        return new HashSet<>(getPreferences(MODE_PRIVATE).getStringSet(PROP_DISABLED_CATEGORIES, new HashSet<String>()));
+    }
+
+    public void setDisabledCategories(Set<String> list) {
+        getPreferences(MODE_PRIVATE).edit().putStringSet(PROP_DISABLED_CATEGORIES, list).apply();
+    }
+
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         Log.d(TAG, "onNavigationItemSelected");
+        String id = item.getIntent().getAction();
+        Set<String> disabled = getDisabledCategories();
+        boolean isDisabled = disabled.contains(id);
+        setupNavItem(item, id, !isDisabled);
+        if(isDisabled) {
+            disabled.remove(id);
+        } else {
+            disabled.add(id);
+        }
+        setDisabledCategories(disabled);
         return false;
+    }
+
+    private void setupNavItem(MenuItem item, String catId, boolean disabled) {
+        if(!disabled) {
+            item.setChecked(true);
+            item.setIcon(R.drawable.ic_done_black_24dp);
+        } else {
+            item.setChecked(false);
+            item.setIcon(R.drawable.ic_clear_black_24dp);
+        }
     }
 
     @Override
@@ -134,4 +178,9 @@ public class MainActivity extends AppCompatActivity
         Log.d(TAG, "onStart");
         super.onStart();
     }
+
+    public static final String[] categories_id = {"28", "12", "16", "35", "80", "99", "18", "10751", "14", "36", "27", "10402", "9648",
+            "10749", "878", "10770", "53", "10752", "37"};
+    public static final String[] categories_names = {"Action", "Adventure", "Animation", "Comedy", "Crime", "Documentary", "Drama", "Family",
+            "Fantasy", "History", "Horror", "Music", "Mystery", "Romance", "Science Fiction", "TV Movie", "Thriller", "War", "Western"};
 }
